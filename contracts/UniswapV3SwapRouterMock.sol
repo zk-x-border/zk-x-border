d9b7770ca91fd064d1a2a6d8a61bb7a74e0aca93
Richard Liang <richardliang2015@u.northwestern.edu>
2023-07-22 22:45:15 +0200

    Add mock uni v3 contract

---
+++ contracts/UniswapV3SwapRouterMock.sol
@@ -0,0 +1,35 @@
+pragma solidity =0.7.6;
+pragma abicoder v2;
+
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "hardhat/console.sol";
+
+contract UniswapV3SwapRouterMock {
+  constructor(
+    IERC20 _usdc,
+    IERC20 _euro
+  ) {
+    usdc = _usdc;
+    euro = _euro;
+  }
+
+  function updateUSDC(IERC20 _usdc) public {
+    usdc = _usdc;
+  }
+
+  function updateEuro(IERC20 _euro) public {
+    euro = _euro;
+  }
+
+  function exactInputSingle(
+    ISwapRouter.ExactInputSingleParams memory params
+  ) external payable returns (uint256 amountOut) {
+    // Transfers USDC from the taker
+    usdc.transferFrom(msg.sender, address(this), params.amountIn);
+
+    // Mints fake EURO to the taker
+    euro.mint(msg.sender, params.amountIn);
+
+    return params.amountIn;
+  }
+}
